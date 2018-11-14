72b0f59727778d42e32d37e9e05d6494bde9755f
TopEpp <natchapol.prms@gmail.com>
2018-11-14 14:02:58 +0700

    add token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'aQFSpeeWpJ2PhGbo7E3HHe4XAYXazxnkRPD6wv/98XcTZ0VduLhv2XF61NSEHOzlUMDbXvyznyVIiNoKg5WD8LP6pzxYiUGprqCx9D2VTxW2yMk6tsO0t4lLJNHU++rjdB8/A7Hnz/sDTb6PI7wp4AdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
